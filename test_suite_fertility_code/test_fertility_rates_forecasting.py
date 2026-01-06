7d2349f5ec895664bceaf15fb1a70460a0996e3f
Abrar Nadeem <abrarnadeem007@gmail.com>
2026-01-06 22:22:50 +0000

    Add test file

---
+++ test_suite_fertility_code/test_fertility_rates_forecasting.py
@@ -0,0 +1,27 @@
+import unittest
+import os
+import pandas as pd
+from fertility_code import fertility_rates_forecasting
+
+class TestFertilityRateForecasting(unittest.TestCase):
+    def test_csv_file(self):
+        # File that is supposed to exist for testing
+        self.existing_file = "fertility_rate.csv"
+
+        # A file that is NOT supposed to exist
+        self.non_existent_file = "this_file_is_not_real.csv"
+
+        # Test loading a CSV file that exists returns a DataFrame
+        existing_df = fertility_rates_forecasting.load_csv_file(self.existing_file)
+        
+        self.assertIsInstance(existing_df, pd.DataFrame)
+        self.assertFalse(existing_df.empty, "DataFrame loaded should not be empty")
+
+        with self.assertRaises(FileNotFoundError):
+            # Test loading a non-existent file raises FileNotFoundError
+            fertility_rates_forecasting.load_csv_file(self.non_existent_file)
+
+if __name__ == "__main__":
+    unittest.main()
+
+
